b7dabb3be88651e71ddd32aeb36c1cec1703ff21
theJ2C3 <48209744+theJ2C3@users.noreply.github.com>
2022-12-09 14:27:44 +0800

    Update settings.py

--- EEG_risk/settings.py
+++ EEG_risk/settings.py
@@ -1,5 +1,7 @@
 from os import environ
 
+setnum=42
+
 
 SESSION_CONFIGS = [
     dict(
@@ -7,54 +9,63 @@ SESSION_CONFIGS = [
         display_name="risk_LC_guide",
         app_sequence=['risk_LC_guide'],
         num_demo_participants=3,
+        num_of_round = setnum,
     ),
     dict(
         name='risk_BDM_guide',
         display_name="risk_BDM_guide",
         app_sequence=['risk_BDM_guide'],
         num_demo_participants=3,
+        num_of_round = setnum,
     ),
     dict(
         name='risk_QSR_guide',
         display_name="risk_QSR_guide",
         app_sequence=['risk_QSR_guide'],
         num_demo_participants=3,
+        num_of_round = setnum,
     ),
     dict(
         name='risk_LC_main_game',
         display_name="risk_LC_main_game",
         app_sequence=['risk_LC_main_game'],
         num_demo_participants=3,
+        num_of_round = setnum,
     ),
     dict(
         name='risk_BDM_main_game',
         display_name="risk_BDM_main_game",
         app_sequence=['risk_BDM_main_game'],
         num_demo_participants=3,
+        num_of_round = setnum,
     ),
     dict(
         name='risk_QSR_main_game',
         display_name="risk_QSR_main_game",
         app_sequence=['risk_QSR_main_game'],
         num_demo_participants=3,
+        num_of_round = setnum,
     ),
     dict(
         name='risk_LC_whole_game',
         display_name="risk_LC_whole_game",
         app_sequence=['risk_LC_guide', 'risk_LC_main_game', 'payment_info'],
         num_demo_participants=3,
+        num_of_round = setnum,
     ),
     dict(
         name='risk_BDM_whole_game',
         display_name="risk_BDM_whole_game",
         app_sequence=['risk_BDM_guide', 'risk_BDM_main_game', 'payment_info'],
         num_demo_participants=3,
+        num_of_round = setnum,
     ),
     dict(
         name='risk_QSR_whole_game',
         display_name="risk_QSR_whole_game",
         app_sequence=['risk_QSR_guide', 'risk_QSR_main_game', 'payment_info'],
         num_demo_participants=3,
+        num_of_round = setnum,
     ),
 ]
 
